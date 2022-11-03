69d910121a83cb0cbab22cfd4fb4056d854d0fb3
jyothivmatam <jyothiv@moback.com>
2022-11-04 00:57:59 +0530

    only get operation done

---
+++ Employeexyz/Controllers/EmployeeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Xml.Linq;
+//using Employeexyz.Model;
+
+
+namespace Employeexyz.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmployeeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public JsonResult Get()
+        {
+            List<Employee> employees = new List<Employee>();
+            using (SqlConnection con = new SqlConnection(_configuration.GetConnectionString("MasterDatabase"))) 
+            //using (SqlConnection con = new SqlConnection(confgjs))
+
+            {
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "select * from Employee";
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dataTable = new DataTable();
+                con.Open();
+                adapter.Fill(dataTable);
+                con.Close();
+                foreach (DataRow dr in dataTable.Rows)
+                {
+                    employees.Add(new Employee
+                    {
+                        EmployeeId = Convert.ToInt32(dr["EmployeeId"]),
+                        EmployeeName = dr["EmployeeName"].ToString(),
+                        EmployeeEmail = dr["EmployeeEmail"].ToString(),
+                    });
+                }
+            }
+            return new JsonResult(employees);
+        }
+
+
+
+
+    }
+}
+
---
+++ Employeexyz/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace Employeexyz
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; } 
+        public string EmployeeName { get; set; } = string.Empty;
+        public string EmployeeEmail { get; set; } = string.Empty;
+    }
+}
